7845c28bc5e4ee796861973275969e3885772323
Daniel Filipe Silva <dfilipe360@gmail.com>
2016-01-07 17:46:07 +0000

    In jenkins-job-status.py script, the data is now stored in a SQLite database (if database file does not exist, it is created at runtime).

--- jenkins-script/jenkins-job-status.py
+++ jenkins-script/jenkins-job-status.py
@@ -3,18 +3,29 @@
 import sys
 import ast
 import urllib
-
-from datetime import datetime
+import sqlite3
 
 JENKINS_BASE_URL = "https://jenkins.qa.ubuntu.com"
-#JENKINS_BASE_URL = "http://192.168.10.10:8080"
 
 def main():
+    # Retrieve the data from the Jenkins instance
     jenkins_data = ast.literal_eval(urllib.urlopen(JENKINS_BASE_URL + "/api/python?tree=jobs[name,color]").read())
-    current_time = str(datetime.now()) # Get the current time string once and use it when it is necessary
     
-    for job in jenkins_data['jobs']:
-        print "(" + current_time + ") " + job['name'] + " - " + job['color']
-    
+    con = sqlite3.connect("jobs-status.db")
+    with con:
+        cursor = con.cursor()
+        
+        # Create table on the SQLite database, if it does not exists
+        cursor.execute("CREATE TABLE IF NOT EXISTS status(id INTEGER PRIMARY KEY AUTOINCREMENT, job TEXT, status TEXT, timestamp TEXT)")
+        
+        for job in jenkins_data['jobs']:
+            # Print to the console the jobs and their status
+            print job['name'] + " - " + job['color'] 
+            
+            # Store in the SQLite database the current job, status and current timestamp
+            cursor.execute("INSERT INTO status(job, status, timestamp) VALUES(?, ?, datetime('now'))", (job['name'], job['color']))
+            
+        print "Total jobs: " + str(len(jenkins_data['jobs']))
+        
 if __name__ == "__main__":
     main()
